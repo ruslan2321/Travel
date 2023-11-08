ae8d5d16721c32c7bb6d262afc670a46bc6e4188
alaska <rus.rakhmatullin.06@bk.ru>
2023-11-08 23:30:44 +0300

    1

---
+++ script.js
@@ -0,0 +1,16 @@
+let slid_2 = document.getElementById('slide_2')
+let slid_1 = document.getElementById('slide_2')
+let cir_1 = document.getElementById('cir_1')
+let cir_2 = document.getElementById('cir_2')
+const Next = () =>{
+    slide_2.style.opacity= "1";
+    slide_1.style.opacity= "0";
+    cir_1.style.backgroundColor = ' rgba(0, 0, 0, 0.386)';
+    cir_2.style.backgroundColor = '#0499DD';
+}
+const Prew = () =>{
+    slide_2.style.opacity = '0';
+    slide_1.style.opacity= "1";
+    cir_2.style.backgroundColor = ' rgba(0, 0, 0, 0.386)';
+    cir_1.style.backgroundColor = '#0499DD';
+}
